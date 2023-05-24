fea874e94695c798dab92e284ec68cfe4f23edfd
MrJambix <131601090+MrJambix@users.noreply.github.com>
2023-05-24 19:42:50 +0200

    Create DRK MT DSW.lua

---
+++ TimelineReactions/MrJambix/DSW(DSR)/DRK MT DSW.lua
@@ -0,0 +1,146 @@
+local tbl = 
+{
+	[3] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Variable",
+							gVar = "ACR_RikuDRK2_Tankbar_DarkMind",
+							uuid = "0db2f23f-74f2-08a7-84cd-392f03f0e851",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 10,
+				name = "Dark Mind",
+				timeRange = true,
+				timelineIndex = 3,
+				timerEndOffset = -1,
+				timerStartOffset = -5,
+				uuid = "cc97a71c-4399-562a-a6c5-2f8d5c52583e",
+				version = 2,
+			},
+		},
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Variable",
+							gVar = "ACR_RikuDRK2_Tankbar_Reprisal",
+							uuid = "e7b35844-a64a-f170-8ea6-55c322e42879",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 10,
+				name = "Reprisal",
+				timeRange = true,
+				timelineIndex = 3,
+				timerEndOffset = -1,
+				timerStartOffset = -6,
+				uuid = "5ae5e38e-5924-26f4-bb90-e64f3157bcf4",
+				version = 2,
+			},
+		},
+	},
+	[4] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Variable",
+							gVar = "ACR_RikuDRK2_Tankbar_Rampart",
+							uuid = "e7da1f9e-bc81-e20c-a6cf-aac82c39f758",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 23.2,
+				name = "Rampart",
+				timeRange = true,
+				timelineIndex = 4,
+				timerEndOffset = -4,
+				timerOffset = -12,
+				timerStartOffset = -12,
+				uuid = "30154139-7ae6-efea-a2c0-82e2b7cd216f",
+				version = 2,
+			},
+		},
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Variable",
+							gVar = "ACR_RikuDRK2_Tankbar_OblationSelf",
+							uuid = "da5fadb8-02ce-67fc-b6ef-2e6bfd19e7f8",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 23.2,
+				name = "Oblation",
+				randomOffset = -1,
+				timeRange = true,
+				timelineIndex = 4,
+				timerEndOffset = -1,
+				timerOffset = -8,
+				timerStartOffset = -8,
+				uuid = "9340ac7c-adb4-bae1-81ae-87791756433c",
+				version = 2,
+			},
+			inheritedIndex = 9,
+		},
+	},
+	inheritedProfiles = 
+	{
+	},
+	mapID = 968,
+	version = 5,
+}
+
+
+
+return tbl
