b560ab0e7f9ed6eeb9903cf06161eb08c512d927
MrJambix <131601090+MrJambix@users.noreply.github.com>
2023-06-04 01:08:07 +0200

    Create War P10S Reaction.lua

---
+++ TimelineReactions/MrJambix/War P10S Reaction.lua
@@ -0,0 +1,169 @@
+local tbl = 
+{
+	[3] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Variable",
+							actionID = 3626,
+							gVar = "ACR_RikuWAR2_Tankbar_Reprisal",
+							ignoreWeaveRules = true,
+							uuid = "92d31c24-e0b5-c6ab-837f-549d83f58571",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 14.1,
+				name = "Reprisal",
+				timelineIndex = 3,
+				timerOffset = -2,
+				uuid = "b4d689b2-4030-e461-83d3-10efa63a1d4a",
+				version = 2,
+			},
+		},
+	},
+	[6] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Variable",
+							gVar = "ACR_RikuWAR2_Tankbar_ThrillOfBattle",
+							uuid = "0a4be300-273a-e571-a908-da05fa2ace03",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 40.8,
+				name = "Thrill",
+				timeRange = true,
+				timelineIndex = 6,
+				timerEndOffset = 3,
+				timerOffset = -2,
+				timerStartOffset = -1,
+				uuid = "5ca057d8-17a9-97cb-ad7f-a30e99662a3f",
+				version = 2,
+			},
+		},
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 7388,
+							ignoreWeaveRules = true,
+							uuid = "442b2c5a-2019-fc52-83bf-b7be269fd9a9",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 40.8,
+				name = "Shake It Off",
+				timelineIndex = 6,
+				timerOffset = 5,
+				uuid = "df504ec2-511d-e033-b05a-f025ee406785",
+				version = 2,
+			},
+		},
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 25751,
+							ignoreWeaveRules = true,
+							uuid = "de378aa3-5a36-7fea-9373-af9222ba8390",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 40.8,
+				name = "Whetting",
+				timelineIndex = 6,
+				timerOffset = -3,
+				uuid = "1b5ca738-cb1c-33bd-92f3-181c04cf7ae0",
+				version = 2,
+			},
+		},
+	},
+	[12] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 25751,
+							ignoreWeaveRules = true,
+							uuid = "f379119a-2f82-c6b0-8287-e849f86cee51",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 70.9,
+				name = "Whetting",
+				timelineIndex = 12,
+				timerOffset = -2,
+				uuid = "9906674f-365c-6435-8e7c-56f8fe3b8fd7",
+				version = 2,
+			},
+		},
+	},
+	inheritedProfiles = 
+	{
+		"store\\anyone\\savage3\\p10s",
+	},
+	mapID = 1150,
+	version = 2,
+}
+
+
+
+return tbl
