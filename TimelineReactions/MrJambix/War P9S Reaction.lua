d0a30283ee5a643917838bca6e30b714782f8998
MrJambix <131601090+MrJambix@users.noreply.github.com>
2023-06-03 18:42:32 +0200

    Create War P9S Reaction.lua

---
+++ TimelineReactions/MrJambix/War P9S Reaction.lua
@@ -0,0 +1,866 @@
+local tbl = 
+{
+	[3] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 25751,
+							atomicPriority = true,
+							ignoreWeaveRules = true,
+							uuid = "543e8848-5856-b8cf-bac3-8381973ea75f",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 12.2,
+				name = "Whetting",
+				timelineIndex = 3,
+				timerOffset = -5,
+				uuid = "b9b7d8b0-419a-e464-a46c-d51fe4690d5a",
+				version = 2,
+			},
+		},
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 7388,
+							atomicPriority = true,
+							ignoreWeaveRules = true,
+							uuid = "d2b853af-4edc-e783-a259-03b247d45b4c",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 12.2,
+				name = "Shake It Off",
+				timelineIndex = 3,
+				timerOffset = -2,
+				uuid = "9e3ebdfc-ba11-d0e4-8f07-9adcd5cc8c73",
+				version = 2,
+			},
+			inheritedIndex = 5,
+		},
+	},
+	[7] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Variable",
+							gVar = "ACR_RikuWAR2_Hotbar_Defiance",
+							uuid = "b6bd6291-9140-4700-bfcd-4e14e0640b49",
+							variableTogglesType = 2,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 47,
+				name = "Stance On",
+				timeRange = true,
+				timelineIndex = 7,
+				timerEndOffset = -10,
+				timerStartOffset = -15,
+				uuid = "0c6f33ec-6c96-3088-8500-ffb37e22a2fa",
+				version = 2,
+			},
+			inheritedIndex = 2,
+		},
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Variable",
+							gVar = "ACR_RikuWAR2_Tankbar_Vengeance",
+							uuid = "049fc875-7866-cfa5-823a-c032f9d1301c",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 47,
+				name = "Vengeance",
+				timeRange = true,
+				timelineIndex = 7,
+				timerEndOffset = -2,
+				timerStartOffset = -7,
+				uuid = "30422c18-1799-9e04-baa1-c9b857864b98",
+				version = 2,
+			},
+		},
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 25751,
+							atomicPriority = true,
+							ignoreWeaveRules = true,
+							uuid = "543e8848-5856-b8cf-bac3-8381973ea75f",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 47,
+				name = "Whetting",
+				timelineIndex = 7,
+				timerOffset = -5,
+				uuid = "35088daa-04b8-703e-bed3-e8e5f6a98c3c",
+				version = 2,
+			},
+		},
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Variable",
+							gVar = "ACR_RikuWAR2_Tankbar_Reprisal",
+							uuid = "f5147048-80d5-3f0a-8b08-0ee69b85d51c",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 47,
+				name = "Reprisal",
+				timeRange = true,
+				timelineIndex = 7,
+				timerEndOffset = -1,
+				timerStartOffset = -7,
+				uuid = "a44ef87c-4354-de14-b98b-a6673cf3c5ba",
+				version = 2,
+			},
+		},
+	},
+	[12] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 1061,
+							atomicPriority = true,
+							gVar = "ACR_RikuWAR2_Tankbar_NascentFlashLowest",
+							ignoreWeaveRules = true,
+							targetType = "Party",
+							uuid = "7b17a29c-1ed4-388e-a8c0-90095774399f",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 68.1,
+				name = "Nascent Flash",
+				timelineIndex = 12,
+				timerOffset = -3,
+				uuid = "17ceaac9-4c0f-33b4-b692-747930ce2912",
+				version = 2,
+			},
+		},
+	},
+	[13] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 1061,
+							atomicPriority = true,
+							gVar = "ACR_RikuWAR2_Tankbar_NascentFlashLowest",
+							ignoreWeaveRules = true,
+							targetType = "Party",
+							uuid = "7b17a29c-1ed4-388e-a8c0-90095774399f",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 68.1,
+				name = "Nascent Flash",
+				timelineIndex = 13,
+				timerOffset = -3,
+				uuid = "68a0a8ba-d3b4-7eac-9218-b5e8cde547a8",
+				version = 2,
+			},
+		},
+	},
+	[16] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Variable",
+							gVar = "ACR_RikuWAR2_Tankbar_Rampart",
+							uuid = "b018013d-7178-3b2e-8c7b-25875473466f",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 81.1,
+				name = "Rampart",
+				timeRange = true,
+				timelineIndex = 16,
+				timerEndOffset = -14,
+				timerStartOffset = -17,
+				uuid = "daebbfac-866e-e159-bd8b-8c93e0b74b84",
+				version = 2,
+			},
+		},
+	},
+	[20] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 1061,
+							atomicPriority = true,
+							gVar = "ACR_RikuWAR2_Tankbar_NascentFlashLowest",
+							ignoreWeaveRules = true,
+							targetType = "Party",
+							uuid = "7b17a29c-1ed4-388e-a8c0-90095774399f",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 94.1,
+				name = "Nascent Flash",
+				timelineIndex = 20,
+				timerOffset = -3,
+				uuid = "92c107b8-bee8-6588-9984-653cf071cd29",
+				version = 2,
+			},
+		},
+	},
+	[21] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 1061,
+							atomicPriority = true,
+							gVar = "ACR_RikuWAR2_Tankbar_NascentFlashLowest",
+							ignoreWeaveRules = true,
+							targetType = "Party",
+							uuid = "7b17a29c-1ed4-388e-a8c0-90095774399f",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 94.1,
+				name = "Nascent Flash",
+				timelineIndex = 21,
+				timerOffset = -3,
+				uuid = "3e914595-e99b-9b3a-a203-4f68e442b67c",
+				version = 2,
+			},
+		},
+	},
+	[26] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 25751,
+							atomicPriority = true,
+							ignoreWeaveRules = true,
+							uuid = "543e8848-5856-b8cf-bac3-8381973ea75f",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 146.4,
+				name = "Whetting",
+				timelineIndex = 26,
+				timerOffset = -5,
+				uuid = "6eba2524-dbf8-8fff-a5c5-80c4f19db9ea",
+				version = 2,
+			},
+		},
+	},
+	[36] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 7388,
+							atomicPriority = true,
+							ignoreWeaveRules = true,
+							uuid = "d2b853af-4edc-e783-a259-03b247d45b4c",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 175.8,
+				name = "Shake It Off",
+				timelineIndex = 36,
+				timerOffset = -29,
+				uuid = "5a0f21e3-ae5f-e61c-a406-a5de873df59e",
+				version = 2,
+			},
+			inheritedIndex = 5,
+		},
+	},
+	[37] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Variable",
+							gVar = "ACR_RikuWAR2_Tankbar_Rampart",
+							uuid = "b018013d-7178-3b2e-8c7b-25875473466f",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 187.1,
+				name = "Rampart",
+				timeRange = true,
+				timelineIndex = 37,
+				timerEndOffset = -14,
+				timerStartOffset = -17,
+				uuid = "09b31d7a-791b-87d9-9d6c-12614472e00b",
+				version = 2,
+			},
+		},
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 25751,
+							atomicPriority = true,
+							ignoreWeaveRules = true,
+							uuid = "543e8848-5856-b8cf-bac3-8381973ea75f",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 187.1,
+				name = "Whetting",
+				timelineIndex = 37,
+				timerOffset = -5,
+				uuid = "9e15c75a-9eb5-20d0-8449-e244835e32f8",
+				version = 2,
+			},
+		},
+	},
+	[70] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 1061,
+							atomicPriority = true,
+							gVar = "ACR_RikuWAR2_Tankbar_NascentFlashLowest",
+							ignoreWeaveRules = true,
+							targetType = "Party",
+							uuid = "7b17a29c-1ed4-388e-a8c0-90095774399f",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 266.2,
+				name = "Nascent Flash",
+				timelineIndex = 70,
+				timerOffset = -2,
+				uuid = "d8d6e337-ca0c-5dd7-84e1-f7a520346c24",
+				version = 2,
+			},
+		},
+	},
+	[92] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Variable",
+							gVar = "ACR_RikuWAR2_Tankbar_Vengeance",
+							uuid = "049fc875-7866-cfa5-823a-c032f9d1301c",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 393.7,
+				name = "Vengeance",
+				timeRange = true,
+				timelineIndex = 92,
+				timerEndOffset = -2,
+				timerStartOffset = -7,
+				uuid = "e177bce8-b435-7c41-8a92-bd4756a850b2",
+				version = 2,
+			},
+		},
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 25751,
+							atomicPriority = true,
+							ignoreWeaveRules = true,
+							uuid = "543e8848-5856-b8cf-bac3-8381973ea75f",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 393.7,
+				name = "Whetting",
+				timelineIndex = 92,
+				timerOffset = -5,
+				uuid = "2083562e-4dfb-580c-9af6-15a782ef2bce",
+				version = 2,
+			},
+		},
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Variable",
+							gVar = "ACR_RikuWAR2_Tankbar_Reprisal",
+							uuid = "f5147048-80d5-3f0a-8b08-0ee69b85d51c",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 393.7,
+				name = "Reprisal",
+				timeRange = true,
+				timelineIndex = 92,
+				timerEndOffset = -1,
+				timerStartOffset = -7,
+				uuid = "2b7de3b8-2af9-531f-b07b-8eb28a66dacd",
+				version = 2,
+			},
+		},
+	},
+	[103] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 1061,
+							atomicPriority = true,
+							gVar = "ACR_RikuWAR2_Tankbar_NascentFlashLowest",
+							ignoreWeaveRules = true,
+							targetType = "Party",
+							uuid = "7b17a29c-1ed4-388e-a8c0-90095774399f",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 420.9,
+				name = "Nascent Flash",
+				timelineIndex = 103,
+				timerOffset = -2,
+				uuid = "e8ca6e30-cdc9-5de5-b493-ffb747abde4f",
+				version = 2,
+			},
+		},
+	},
+	[117] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 1061,
+							atomicPriority = true,
+							gVar = "ACR_RikuWAR2_Tankbar_NascentFlashLowest",
+							ignoreWeaveRules = true,
+							targetType = "Party",
+							uuid = "7b17a29c-1ed4-388e-a8c0-90095774399f",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 471.4,
+				name = "Nascent Flash",
+				timelineIndex = 117,
+				timerOffset = -2,
+				uuid = "2af37f01-4eb5-ffd8-aeec-d234e5086210",
+				version = 2,
+			},
+		},
+	},
+	[125] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 1061,
+							atomicPriority = true,
+							gVar = "ACR_RikuWAR2_Tankbar_NascentFlashLowest",
+							ignoreWeaveRules = true,
+							targetType = "Party",
+							uuid = "7b17a29c-1ed4-388e-a8c0-90095774399f",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 508.2,
+				name = "Nascent Flash",
+				timelineIndex = 125,
+				timerOffset = -2,
+				uuid = "d8ef74bd-51a3-b754-8f81-be520f581399",
+				version = 2,
+			},
+		},
+	},
+	[131] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Variable",
+							gVar = "ACR_RikuWAR2_Tankbar_Vengeance",
+							uuid = "049fc875-7866-cfa5-823a-c032f9d1301c",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 534.8,
+				name = "Vengeance",
+				timeRange = true,
+				timelineIndex = 131,
+				timerEndOffset = -2,
+				timerStartOffset = -7,
+				uuid = "25d030cb-8421-6f68-83e4-ceba2b7b5fc4",
+				version = 2,
+			},
+		},
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 25751,
+							atomicPriority = true,
+							ignoreWeaveRules = true,
+							uuid = "543e8848-5856-b8cf-bac3-8381973ea75f",
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 534.8,
+				name = "Whetting",
+				timelineIndex = 131,
+				timerOffset = -5,
+				uuid = "f2de46b6-7e2d-72e3-80bb-198a741ed4cd",
+				version = 2,
+			},
+		},
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							aType = "Variable",
+							gVar = "ACR_RikuWAR2_Tankbar_Reprisal",
+							uuid = "f5147048-80d5-3f0a-8b08-0ee69b85d51c",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 534.8,
+				name = "Reprisal",
+				timeRange = true,
+				timelineIndex = 131,
+				timerEndOffset = -1,
+				timerStartOffset = -7,
+				uuid = "aee5ebf9-9f53-a93e-9094-65ab47bfb8f0",
+				version = 2,
+			},
+		},
+	},
+	[137] = 
+	{
+		
+		{
+			data = 
+			{
+				actions = 
+				{
+					
+					{
+						data = 
+						{
+							actionID = 1061,
+							atomicPriority = true,
+							gVar = "ACR_RikuWAR2_Tankbar_NascentFlashLowest",
+							ignoreWeaveRules = true,
+							targetType = "Party",
+							uuid = "7b17a29c-1ed4-388e-a8c0-90095774399f",
+							variableTogglesType = 3,
+							version = 2,
+						},
+					},
+				},
+				conditions = 
+				{
+				},
+				mechanicTime = 551.9,
+				name = "Nascent Flash",
+				timelineIndex = 137,
+				timerOffset = -2,
+				uuid = "742a79df-c52a-5abd-affa-375434950e1a",
+				version = 2,
+			},
+		},
+	},
+	inheritedProfiles = 
+	{
+		"store\\anyone\\savage3\\p9s",
+	},
+	mapID = 1148,
+	version = 2,
+}
+
+
+
+return tbl
